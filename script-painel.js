40675ac6efbcd73b78a8de00e754732a583b9f92
lucascunha-web <azlucascu@gmail.com>
2026-02-03 09:42:55 -0300

    Add files via upload

--- script-painel.js
+++ script-painel.js
@@ -620,8 +620,9 @@ function verModulo(moduloId) {
         if(btnApostila) btnApostila.href = "APOSTILA DE ÓPTICA GEOMÉTRICA.pdf";
         
         adicionarAula("Módulo I: Aula 1 - Óptica Geométrica", "10Wb2lQbOtNgYUuF2TU3lRq0cbqHhL-kQ");
-        adicionarAula("Módulo I: Aula 2 - Ópitica Geométrica","1_n0DraC48T4sogFxR3NPngtGQNz7IOki");
+        adicionarAula("Módulo I: Aula 2 - Óptica Geométrica","1_n0DraC48T4sogFxR3NPngtGQNz7IOki");
         adicionarAula("Módulo I: Aula 3 - Óptica Geométrica","1Zjjed9_SZ6Q5JpF-o936z8KSM16lxVGR");
+        adicionarAula("Módulo I: Aula 4 - Óptica Geométrica", "14LEBDW2SoCoe63LfwFvFHSMVaTOqsCSI");
         
         // adicionarAula("Módulo I: Aula 2 - Exemplo", "ID_VIDEO");
     } 
